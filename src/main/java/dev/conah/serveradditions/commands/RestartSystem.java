d50f197408d7939f0f50285811ed9ff113ed08d4
ConaII <72966048+ConaII@users.noreply.github.com>
2022-01-06 18:31:42 -0300

    Fixed RestartSystem.java

--- src/main/java/dev/conah/serveradditions/commands/RestartSystem.java
+++ src/main/java/dev/conah/serveradditions/commands/RestartSystem.java
@@ -1,5 +1,6 @@
 package dev.conah.serveradditions.commands;
 
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.command.Command;
@@ -8,12 +9,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Objective;
-
 import java.util.Arrays;
 import org.jetbrains.annotations.NotNull;
 
-import dev.conah.serveradditions.ServerAdditions;
 
+import dev.conah.serveradditions.ServerAdditions;
 import static dev.conah.serveradditions.utils.PluginUtils.*;
 import static dev.conah.serveradditions.utils.Variables.*;
 
@@ -54,7 +54,7 @@ public class RestartSystem implements CommandExecutor {
                                 Bukkit.getScheduler().runTaskLater(plugin, () -> broadcast("&8[&6Historika&8] &cEl servidor se cerrara en 4 segundos."), 20*2);
                                 Bukkit.getScheduler().runTaskLater(plugin, () -> broadcast("&8[&6Historika&8] &cEl servidor se cerrara en 3 segundos."), 20*3);
                                 Bukkit.getScheduler().runTaskLater(plugin, () -> broadcast("&8[&6Historika&8] &cEl servidor se cerrara en 2 segundos."), 20*4);
-                                Bukkit.getScheduler().runTaskLater(plugin, () -> broadcast("&8[&6Historika&8] &cEl servidor se cerrara en 1 segundos."), 20*5);
+                                Bukkit.getScheduler().runTaskLater(plugin, () -> broadcast("&8[&6Historika&8] &cEl servidor se cerrara en 1 segundo."), 20*5);
                                 Bukkit.getScheduler().runTaskLater(plugin, () -> {
                                     broadcast("");
                                     broadcast("&8[&6Historika&8] &cCerrando el servidor...");
@@ -77,32 +77,75 @@ public class RestartSystem implements CommandExecutor {
                     return true;
                 }
                 if(args[0].equalsIgnoreCase("-m")) {
-                    score.setScore(1);
-                    StringBuilder message = new StringBuilder();
-                    for (int i = 1; i < args.length; i++){
-                        message.append(" ").append(Arrays.toString(args));
-                    }
+                    if(args.length > 1) {
+                        score.setScore(1);
 
-                    broadcast("");
-                    broadcast("&3"+message);
-                    broadcast("&8[&6Historika&8] &cCerrando el servidor...");
-                    broadcast("");
-                    score.setScore(0);
+                        String msg = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+                        /*StringBuilder message = new StringBuilder();
+                        for (int i = 1; i < args.length; i++){
+                            message.append(args[i]).append(" ");
+
+                        }
+                        String msg = message.toString();*/
+                        broadcast("");
+                        broadcast("&3"+msg);
+                        broadcast("&8[&6Historika&8] &cCerrando el servidor...");
+                        broadcast("");
+                        score.setScore(0);
+                    } else {
+                        sender.sendMessage("§4>> §cArgumentos insuficientes.");
+                    }
+                    //sendConsole("execute run stop");
+                    return true;
+                }
+                if(args[0].equalsIgnoreCase("-d")) {
+                    if(args.length > 1) {
+                        if(StringUtils.isNumeric(args[1])) {
+                            int delay = Integer.valueOf(args[1]);
+
+                            if(args[2].equalsIgnoreCase("-m")) {
+                                if(args.length > 3) {
+                                    score.setScore(1);
+                                    String msg = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+                                    broadcast("");
+                                    broadcast("&3"+msg);
+                                    broadcast("&8[&6Historika&8] &cEl servidor se cerrara en"+delay+" segundos.");
+                                    broadcast("");
+
+                                    broadcast("");
+                                    broadcast("&8[&6Historika&8] &cCerrando el servidor...");
+                                    broadcast("");
+                                } else {
+                                    sender.sendMessage("§4>> §cArgumentos insuficientes.");
+                                }
+                            }
+                            score.setScore(0);
+                        } else {
+                            sender.sendMessage("§4>> §cEl argumento delay debe ser un numero!");
+                        }
+
+                    } else {
+                        sender.sendMessage("§4>> §cArgumentos insuficientes.");
+                    }
                     //sendConsole("execute run stop");
                     return true;
                 }
                 if(args[0].equalsIgnoreCase("help")){
                     sender.sendMessage("");
                     sender.sendMessage(_PREFIX_+" §cValid commands:");
-                    sender.sendMessage("§e  ");
+                    sender.sendMessage("§e /se_restart (Cooldown 60s)");
+                    sender.sendMessage("§e /se_restart -m <message>");
+                    sender.sendMessage("§e /se_restart -d <seconds>");
+                    sender.sendMessage("§e /se_restart -d <seconds> -m <message>");
+                    sender.sendMessage("§e /se_restart -fast");
                     return true;
                 }
             } else {
                 sender.sendMessage("");
-                sender.sendMessage("§4>> &cEl servidor ya se esta cerrando!");
+                sender.sendMessage("§4>> §cEl servidor ya se esta cerrando!");
                 return true;
             }
-            return true;
+            return false;
         }
         return true;
     }
