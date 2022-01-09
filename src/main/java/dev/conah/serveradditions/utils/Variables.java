88ec05be108a48bf942ad2771423c28fc7f69040
ConaII <72966048+ConaII@users.noreply.github.com>
2022-01-09 19:54:25 -0300

    Update Variables.java

--- src/main/java/dev/conah/serveradditions/utils/Variables.java
+++ src/main/java/dev/conah/serveradditions/utils/Variables.java
@@ -5,13 +5,21 @@ import org.bukkit.Bukkit;
 import org.bukkit.Server;
 import org.bukkit.command.ConsoleCommandSender;
 
+import java.util.List;
+
 public class Variables {
     private static final ServerAdditions plugin = ServerAdditions.inst();
 
+    public static String _NAME_ = plugin.getDescription().getName();
     public static String _PREFIX_ = plugin.getDescription().getPrefix();
     public static String _VERSION_ = plugin.getDescription().getVersion();
+    public static String _APIVERSION_ = plugin.getDescription().getAPIVersion();
+    public static List<String> _AUTHORS_ = plugin.getDescription().getAuthors();
 
-    public static String _CPREFIX_ = plugin.getConfig().getString("");
+
+    public static String _CPREFIX_ = plugin.getConfig().getString("general-config.prefix");
+    public static String _LANGUAGE_ = plugin.getConfig().getString("general-config.language");
+
 
     public static Server server = Bukkit.getServer();
     public static ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
