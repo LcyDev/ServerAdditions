92d3732892ee75b273ccfa5670e3ed1e28b76d4d
ConaII <72966048+ConaII@users.noreply.github.com>
2022-01-09 19:54:30 -0300

    Update Config.java

--- src/main/java/dev/conah/serveradditions/utils/Config.java
+++ src/main/java/dev/conah/serveradditions/utils/Config.java
@@ -1,13 +1,24 @@
 package dev.conah.serveradditions.utils;
 
 import dev.conah.serveradditions.ServerAdditions;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.util.List;
 
 public class Config {
 
+    private Config() {
+        if(!messagesFile.exists()){
+            plugin.saveResource("messages.yml", true);
+        }
+    }
+
     private static final ServerAdditions plugin = ServerAdditions.inst();
 
-
-    public static String getConfig(String path) {
-        return plugin.getConfig().getString(path);
-    }
+    public static File messagesFile = new File(plugin.getDataFolder()+"/messages.yml");
+    public static YamlConfiguration msgYML = YamlConfiguration.loadConfiguration(messagesFile);
+    public static FileConfiguration configYML = plugin.getConfig();
 }
